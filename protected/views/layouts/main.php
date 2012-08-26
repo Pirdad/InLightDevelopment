5ae3843354e580ed34c3f572a9384ffeacfa5c23
Pirdad <Sakhizada>
2012-08-26 17:53:45 -0400

    logo and menu button icons added.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,4 +1,36 @@
-<?php /* @var $this Controller */ ?>
+<?php /* @var $this Controller */ 
+
+function getSelected($button) {
+	
+	if (Yii::app()->getController()->id == "site") 
+	{
+		$page = Yii::app()->getController()->getAction()->id;
+		if ($button == $page) {
+			return "_selected";
+		} else if ($page == "index" && $button == "home") {
+			return "_selected";
+		} else {
+			return "";
+		}
+	}
+}
+
+function getButtonImage($button) {
+	
+	if ($button == "home") {
+		return  Yii::app()->request->baseUrl.'/images/Buttons/home_btn'.getSelected($button).'.png';
+	} else if ($button == "biography") {
+		return  Yii::app()->request->baseUrl.'/images/Buttons/bio_btn'.getSelected($button).'.png';
+	} else if ($button == "services") {
+		return  Yii::app()->request->baseUrl.'/images/Buttons/services_btn'.getSelected($button).'.png';
+	} else if ($button == "portfolio") {
+		return  Yii::app()->request->baseUrl.'/images/Buttons/portfolio_btn'.getSelected($button).'.png';
+	} else if ($button == "contact") {
+		return  Yii::app()->request->baseUrl.'/images/Buttons/contact_btn'.getSelected($button).'.png';
+	}
+}
+
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
@@ -23,20 +55,25 @@
 <div class="container" id="page">
 
 	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
+		<div id="logo"><img src="<?php echo Yii::app()->request->baseUrl;?>/images/inLightDevelopment.png" /></div>
+		<div id="mainmenu">
+			<ul>
+				<li>
+					<a href="<?php echo Yii::app()->request->baseUrl;?>/site/index" ><img src="<?php echo getButtonImage('home'); ?>" /></a>
+				</li><li>
+					<a href="" ><img src="<?php echo getButtonImage('biography'); ?>" /></a>
+				</li><li>
+					<a href="" ><img src="<?php echo getButtonImage('services'); ?>" /></a>
+				</li><li>
+					<a href="" ><img src="<?php echo getButtonImage('portfolio'); ?>" /></a>
+				</li><li>
+					<a href="<?php echo Yii::app()->request->baseUrl;?>/site/contact" ><img src="<?php echo getButtonImage('contact'); ?>" /></a>
+				</li>	
+			</ul>
+		</div><!-- mainmenu -->
 	</div><!-- header -->
 
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-			),
-		)); ?>
-	</div><!-- mainmenu -->
+	
 	<?php if(isset($this->breadcrumbs)):?>
 		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
 			'links'=>$this->breadcrumbs,
