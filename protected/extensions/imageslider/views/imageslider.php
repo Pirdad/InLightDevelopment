c67a32e4bfbf8d861dfae058175c7b2ab2ba644c
Pirdad <Sakhizada>
2012-09-09 14:36:12 -0400

    Major Features added to the Imageslider extension.
    ImageSlider's width, height, buttons can be customized. Also removed the
    created assets folder.

--- protected/extensions/imageslider/views/imageslider.php
+++ protected/extensions/imageslider/views/imageslider.php
@@ -10,27 +10,25 @@ for ($i=0; $i<count($this->images); $i++) {
 
 $size = count($this->images);
 echo "<input type='hidden' id='".$IMG_SLIDER_ID."size' value='$size' />";
-if ($this->back_button) {
-	
-}
-if ($this->back_button_hover) {
-	
-}
-if ($this->forward_button) {
-	
-}
-if ($this->forward_button_hover) {
-	
-}
-echo "<input type='hidden' id='".$IMG_SLIDER_ID."back' value='$this->back_button' />";
-echo "<input type='hidden' id='".$IMG_SLIDER_ID."back_hover' value='$this->back_button_hover' />";
-echo "<input type='hidden' id='".$IMG_SLIDER_ID."forward' value='$this->forward_button' />";
-echo "<input type='hidden' id='".$IMG_SLIDER_ID."forward_hover' value='$this->forward_button_hover' />";
+
+echo "<input type='hidden' id='".$IMG_SLIDER_ID."back' value='$this->back_button_image' />";
+echo "<input type='hidden' id='".$IMG_SLIDER_ID."back_hover' value='$this->back_button_hover_image' />";
+echo "<input type='hidden' id='".$IMG_SLIDER_ID."forward' value='$this->forward_button_image' />";
+echo "<input type='hidden' id='".$IMG_SLIDER_ID."forward_hover' value='$this->forward_button_hover_image' />";
+
+echo "<input type='hidden' id='".$IMG_SLIDER_ID."overall_width' value='$this->overall_width' />";
+echo "<input type='hidden' id='".$IMG_SLIDER_ID."overall_height' value='$this->overall_height' />";
+echo "<input type='hidden' id='".$IMG_SLIDER_ID."back_width' value='$this->back_button_width' />";
+echo "<input type='hidden' id='".$IMG_SLIDER_ID."back_height' value='$this->back_button_height' />";
+echo "<input type='hidden' id='".$IMG_SLIDER_ID."forward_width' value='$this->forward_button_width' />";
+echo "<input type='hidden' id='".$IMG_SLIDER_ID."forward_height' value='$this->forward_button_height' />";
 
 ?>
 
-<div id="img_slider_back_btn" class="slider_button unselectable_box" ></div>
-
-<div id="img_slider_image_holder" class="unselectable_box" ></div>
-
-<div id="img_slider_forward_btn" class="slider_button unselectable_box" ></div>
+<div id="img_slider_container">
+	<div id="img_slider_back_btn" class="slider_button unselectable_box" ></div>
+	
+	<div id="img_slider_image_holder" class="unselectable_box" ></div>
+	
+	<div id="img_slider_forward_btn" class="slider_button unselectable_box" ></div>
+</div>
