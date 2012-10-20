186d3ffa86ece454fb62036fa4f41dfa7c5026da
Najeeb <ns.jeeb@gmail.com>
2012-10-19 22:33:58 -0400

    tested and add more picturs

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -28,11 +28,14 @@ Yii::import("ext.imagegallery.ImageGallery", true);
 
 //Yii::import("ext.imageslider.ImageSlider", true);
 $images = array(
-		Yii::app()->baseUrl."/images/Home/Features/free_website_promo.png",
+		//Yii::app()->baseUrl."/images/Home/Features/free_website_promo.png",
+		Yii::app()->baseUrl."/images/Home/sample_aziz_reno.png",
+		Yii::app()->baseUrl."/images/Home/sample_inlight_dev.png",
+		Yii::app()->baseUrl."/images/inLightLogo.png",
 		Yii::app()->baseUrl."/images/Home/sample_aziz_reno.png",
 		Yii::app()->baseUrl."/images/Home/sample_inlight_dev.png",
 		Yii::app()->baseUrl."/images/inLightLogo.png");
 
 $this->widget("ext.imagegallery.ImageGallery", array(
-	ImageGallery::$COLUMNS =>5, ImageGallery::$ROWS=>3, ImageGallery::$IMAGES=>$images));
+	ImageGallery::$COLUMNS =>4, ImageGallery::$ROWS=>2, ImageGallery::$IMAGES=>$images));
 ?>
