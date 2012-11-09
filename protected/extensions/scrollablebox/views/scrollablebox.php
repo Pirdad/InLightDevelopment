b49a3dec16bb661b7335d54c7bb7de7dda44627e
Pirdad <Sakhizada>
2012-11-08 22:21:58 -0500

    Finished building the scrollable box widget for Yii Framework. Very customizable.

--- protected/extensions/scrollablebox/views/scrollablebox.php
+++ protected/extensions/scrollablebox/views/scrollablebox.php
@@ -13,21 +13,21 @@ $SCROLLABLE_BOX = "scrollable_box_";
 	<div id="<?php echo $SCROLLABLE_BOX; ?>content_container" >
 	
 		<div id="<?php echo $SCROLLABLE_BOX; ?>content" >
-				<p>Lorem   ipsum   dolor   sit   amet,   consectet
-				adipiscing   elit. Maecenas    feugiat j  usto   
-				mauris   commodo    adipiscing. 
-				Fusce ac  diam  sit amet  sem vehicula moles
-				Integer aliquet viverra  metus  euismod temp
-				Proin consequat pellentesque dolor ut biben
-				Duis eget lectus quam. Duis id leo sed justo 
-				aliquet     volutpat.   Mauris    ut l  acus   ege.
-				</p>
+				<?php echo $this->html_content; ?>
 		</div>
 		
 		<div id="<?php echo $SCROLLABLE_BOX; ?>scrollbar_container" >
-			<div id="<?php echo $SCROLLABLE_BOX; ?>scrollbar"></div>
+			<div id="<?php echo $SCROLLABLE_BOX; ?>scrollbar" ></div>
 		</div>
+		
+		<div id="<?php echo $SCROLLABLE_BOX; ?>scrollable_indicator" 
+			class="unselectable" >
+			SCROLLABLE
+		</div>
+	</div>
 	
+	<div id="<?php echo $SCROLLABLE_BOX; ?>footer" >
+		Footer
 	</div>
 
 </div>
--- protected/views/site/services.php
+++ protected/views/site/services.php
@@ -11,10 +11,82 @@ $this->breadcrumbs=array(
 <h4>This page is under construction and coming soon</h4>
 
 <?php
+$html_content = 
+<<<HTML
+	<div style="padding-top:10px; padding-bottom:10px; padding-right:10px; padding-left:10px;">
+				<p style="background-color: #84BD99;" >Lorem   ipsum   dolor   sit   amet,   consectet
+				adipiscing   elit. Maecenas    feugiat j  usto   
+				mauris   commodo    adipiscing. 
+				Fusce ac  diam  sit amet  sem vehicula moles
+				Integer aliquet viverra  metus  euismod temp
+				Proin consequat pellentesque dolor ut biben
+				Duis eget lectus quam. Duis id leo sed justo 
+				aliquet     volutpat.   Mauris    ut l  acus   ege.
+				</p>
+				<p style="background-color: #84BD99;" >Lorem   ipsum   dolor   sit   amet,   consectet
+				adipiscing   elit. Maecenas    feugiat j  usto   
+				mauris   commodo    adipiscing. 
+				Fusce ac  diam  sit amet  sem vehicula moles
+				Integer aliquet viverra  metus  euismod temp
+				Proin consequat pellentesque dolor ut biben
+				Duis eget lectus quam. Duis id leo sed justo 
+				aliquet     volutpat.   Mauris    ut l  acus   ege.
+				</p>
+				<p style="background-color: #84BD99;" >Lorem   ipsum   dolor   sit   amet,   consectet
+				adipiscing   elit. Maecenas    feugiat j  usto   
+				mauris   commodo    adipiscing. 
+				Fusce ac  diam  sit amet  sem vehicula moles
+				Integer aliquet viverra  metus  euismod temp
+				Proin consequat pellentesque dolor ut biben
+				Duis eget lectus quam. Duis id leo sed justo 
+				aliquet     volutpat.   Mauris    ut l  acus   ege.
+				</p>
+				<p style="background-color: #84BD99;" >Lorem   ipsum   dolor   sit   amet,   consectet
+				adipiscing   elit. Maecenas    feugiat j  usto   
+				mauris   commodo    adipiscing. 
+				Fusce ac  diam  sit amet  sem vehicula moles
+				Integer aliquet viverra  metus  euismod temp
+				Proin consequat pellentesque dolor ut biben
+				Duis eget lectus quam. Duis id leo sed justo 
+				aliquet     volutpat.   Mauris    ut l  acus   ege.
+				</p>
+				<p style="background-color: #84BD99;" >Lorem   ipsum   dolor   sit   amet,   consectet
+				adipiscing   elit. Maecenas    feugiat j  usto   
+				mauris   commodo    adipiscing. 
+				Fusce ac  diam  sit amet  sem vehicula moles
+				Integer aliquet viverra  metus  euismod temp
+				Proin consequat pellentesque dolor ut biben
+				Duis eget lectus quam. Duis id leo sed justo 
+				aliquet     volutpat.   Mauris    ut l  acus   ege.
+				</p>
+		</div>
+HTML;
 
 Yii::import("ext.scrollablebox.ScrollableBox", true);
 
 $this->widget("ext.scrollablebox.ScrollableBox", array(
-	ScrollableBox::$HTML_CONTENT => '<div>I am crazy!</div>',
+	ScrollableBox::$HTML_CONTENT => $html_content,
+	ScrollableBox::$WIDGET_WIDTH => 800,
+	ScrollableBox::$WIDGET_HEIGHT => 400,
+	ScrollableBox::$WIDGET_COLOR => 'black',	
+	ScrollableBox::$SCROLLBAR_CONTAINER_WIDTH => 20,
+	ScrollableBox::$SCROLLBAR_CONTAINER_COLOR => 'white',
+	ScrollableBox::$SCROLLBAR_WIDTH => 18,
+	ScrollableBox::$SCROLLBAR_COLOR => 'orange',
+	ScrollableBox::$SCROLLBAR_VISIBILITY => true,
+	ScrollableBox::$HEADER_HEIGHT => 40,
+	ScrollableBox::$HEADER_COLOR => 'white',
+	ScrollableBox::$HEADER_VISIBILITY => true,
+	ScrollableBox::$FOOTER_HEIGHT => 50,
+	ScrollableBox::$FOOTER_COLOR => 'orange',
+	ScrollableBox::$FOOTER_VISIBILITY => true,
+	ScrollableBox::$CONTENT_CONTAINER_COLOR => 'green',
+	ScrollableBox::$CONTENT_COLOR => 'yellow',
+	ScrollableBox::$SCROLLABLE_INDICATOR_VISIBILITY => true,
+	ScrollableBox::$SCROLLABLE_INDICATOR_WIDTH => 400,
+	ScrollableBox::$SCROLLABLE_INDICATOR_HEIGHT => 100,
+	ScrollableBox::$SCROLLABLE_INDICATOR_FONT_COLOR => '#ffffff',
+	ScrollableBox::$SCROLLABLE_INDICATOR_COLOR => 'rgba(67,149,0,0.7)',
+	ScrollableBox::$SCROLLABLE_INDICATOR_POSITION => ScrollableBox::$SCROLLABLE_INDICATOR_RIGHT,
 ));
 ?>
